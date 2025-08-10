bf7dd748999491cee50e011b7c8103f598d4501b
PatiphanAK <pakkahadsri@gmail.com>
2025-08-10 13:15:08 +0700

    refile structure

--- crud_proj/src/services/product_service.rs
+++ crud_proj/src/services/product_service.rs
@@ -1,8 +1,8 @@
-use crate::{errors::AppError, models::products::Product, repositories::product_repositories};
+use crate::{errors::AppError, models::products::Product, repositories::product_repositories::get_product};
 use crate::{models::pagination::Pagination};
 use sqlx::PgPool;
 
 pub async fn list_products(pool: &PgPool,pagination: Pagination) -> Result<Vec<Product>, AppError> {
-    let products = product_repositories::get_product_list_repo(pool, pagination).await?;
+    let products = get_product::get_product_list_repo(pool, pagination).await?;
     Ok(products)
 }
